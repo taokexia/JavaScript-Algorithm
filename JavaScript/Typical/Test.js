0d48fe9491b83c2425ffb5987eb0eeaa785af90b
taokexia <694198149@qq.com>
2019-01-19 18:45:28 +0800

    add binarySearch

---
+++ JavaScript/Typical/Test.js
@@ -0,0 +1,5 @@
+// 测试二分查找法
+var bs = require('./binarySearch');
+var arr1 = [0, 1, 2, 3, 4, 5, 10, 23, 44, 66, 88, 99];
+console.log(bs.binarySearch1(arr1, 10));
+console.log(bs.binarySearch2(arr1, 88));
---
+++ JavaScript/Typical/binarySearch.js
@@ -0,0 +1,40 @@
+/*
+ * 二分查找法
+ * 前提条件：数组有序
+ */
+module.exports.binarySearch1 = function binarySearch1(arr, target) {
+    // 在arr[l...r]之中查找target
+    let l = 0;
+    let r = arr.length-1;
+    while(l <= r) {
+        // int mid = Math.floor((r+l)/2);
+        // 防止极端情况下数据溢出
+        var mid = l + Math.floor((r-l)/2);
+        if(arr[mid] === target) {
+            return mid;
+        }
+        if(arr[mid] > target) {
+            r = mid-1;
+        } else {
+            l = mid+1;
+        }
+    }
+    return -1;
+}
+
+// 用递归的方式写二分查找法
+module.exports.binarySearch2 = function binarySearch2(arr, target) {
+    return __binarySearch2(arr, 0, arr.length-1, target);
+}
+
+function __binarySearch2(arr, l, r, target) {
+    if(l > r) return -1;
+    var mid = l + Math.floor((r - l)/2);
+    if(arr[mid] === target)
+        return mid;
+    else if(arr[mid] < target) {
+        return __binarySearch2(arr, mid+1, r, target);
+    } else {
+        return __binarySearch2(arr, l, mid-1, target);
+    }
+}
